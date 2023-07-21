59f2dc8469e0ff83536fe183053a0354783f7e4c
NikhilBadole <120246851+NikhilBadole@users.noreply.github.com>
2023-07-22 01:42:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const item = document.getElementById("colorSelect");
 const button = document.getElementsByTagName("div")[0];
 
 function removeItem(){
-	const value = item.value;
-	for(let i=0;i<item.length;i++){
-		if(item.value === value) item.remove(item.value);
+	const index = item.selectedIndex;
+
+	if(index !== -1){
+		item.remove(index)
 	}
 }
