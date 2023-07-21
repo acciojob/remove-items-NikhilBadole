bc6139b4fdf63df50a9570aa71ced46ab9b39074
NikhilBadole <120246851+NikhilBadole@users.noreply.github.com>
2023-07-22 01:26:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const item = document.getElementById("colorSelect");
+const button = document.getElementsByTagName("div")[0];
+
+function removeItem(){
+	if(item.value) item.remove(item.value);
+}
